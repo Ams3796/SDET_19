0d1b5d7ee554ec257a89897f8a36e0b5e742a169
Ams3796 <anbuamaya1996@gmail.com>
2021-08-25 16:27:45 +0530

    Added

--- src/main/java/genericLibraries/Autoconstant.java
+++ src/main/java/genericLibraries/Autoconstant.java
@@ -2,9 +2,10 @@ package genericLibraries;
 
 public interface Autoconstant {
     
-	String propertyfile="../SDET_19/src/test/resources/CommonData.properties";
-	String xml="../SDET_19/src/test/resources/CommonDAta.xml";
-	String excel="/SDET_19/src/test/resources/Excel.xlsx";
+	public static String DirPath=System.getProperty("user.dir");
+	String propertyfile=DirPath+"../SDET_19/src/test/resources/CommonData.properties";
+	String xml=DirPath+"../SDET_19/src/test/resources/CommonDAta.xml";
+	String excel=DirPath+"/SDET_19/src/test/resources/Excel.xlsx";
 	long implicitwait=20;
 	long explicitwait=15;
 	String Org_childwindow="Accounts";
